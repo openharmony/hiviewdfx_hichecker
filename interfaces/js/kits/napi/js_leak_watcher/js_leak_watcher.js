0d33eff1bb87cceb77cd522ff575afc31e664a8b
lutao <lutao31@huawei.com>
2024-07-09 17:37:52 +0800

    add param check
    Signed-off-by: lutao <lutao31@huawei.com>

--- interfaces/js/kits/napi/js_leak_watcher/js_leak_watcher.js
+++ interfaces/js/kits/napi/js_leak_watcher/js_leak_watcher.js
@@ -18,10 +18,8 @@ let fs = requireNapi('file.fs');
 let hidebug = requireNapi('hidebug');
 let cryptoFramework = requireNapi('security.cryptoFramework');
 
-
-
 const ERROR_CODE_INVALID_PARAM = 401;
-const ERROR_MSG_INVALID_PARAM = 'Parameter error. The file path is invalid.';
+const ERROR_MSG_INVALID_PARAM = 'Parameter error. Please check!';
 
 let errMap = new Map();
 errMap.set(ERROR_CODE_INVALID_PARAM, ERROR_MSG_INVALID_PARAM);
@@ -41,13 +39,87 @@ class BusinessError extends Error {
 
 let enabled = false;
 let gcObjList = [];
-let leakObjList = [];
+
 let watchObjMap = new Map();
 
+const registry = new FinalizationRegistry((hash) => {
+  gcObjList.push(hash);
+});
 const MAX_FILE_NUM = 20;
+
+function flushLeakList() {
+  let leakObjList = [];
+  for (let [key, value] of watchObjMap) {
+    if (!gcObjList.includes(key)) {
+      leakObjList.push(value);
+    }
+  }
+  return leakObjList;
+}
+
+function createHeapDumpFile(fileName, filePath) {
+  hidebug.dumpJsHeapData(fileName);
+  let heapDumpFileName = fileName + '.heapsnapshot';
+  let desFilePath = filePath + '/' + heapDumpFileName;
+  fs.moveFileSync('/data/storage/el2/base/files/' + heapDumpFileName, desFilePath, 0);
+  return getHeapDumpSHA256(desFilePath);
+}
+
+function getHeapDumpSHA256(filePath) {
+  let md = cryptoFramework.createMd('SHA256');
+  let heapDumpFile = fs.openSync(filePath, fs.OpenMode.READ_WRITE);
+  let bufSize = 1024;
+  let readSize = 0;
+  let buf = new ArrayBuffer(bufSize);
+  let readOptions = {
+    offset: readSize,
+    length: bufSize
+  };
+  let readLen = fs.readSync(heapDumpFile.fd, buf, readOptions);
+
+  while (readLen > 0) {
+    md.updateSync({ data: new Uint8Array(buf.slice(0, readLen)) });
+    readSize += readLen;
+    readOptions.offset = readSize;
+    readLen = fs.readSync(heapDumpFile.fd, buf, readOptions);
+  }
+  fs.closeSync(heapDumpFile);
+
+  let digestOutPut = md.digestSync();
+  return Array.from(digestOutPut.data, byte => ('0' + byte.toString(16)).slice(-2)).join('').toUpperCase();
+}
+
+function deleteOldFile(filePath) {
+  let listFileOption = {
+    recursion: false,
+    listNum: 0,
+    filter: {
+      suffix: [".heapsnapshot", ".jsleaklist"],
+      fileSizeOver: 0
+    }
+  };
+  let files = fs.listFileSync(filePath, listFileOption);
+  if (files.length > MAX_FILE_NUM) {
+    const regex = /(\d+)\.(heapsnapshot|jsleaklist)/;
+    files.sort((a, b) => {
+      const matchA = a.match(regex);
+      const matchB = b.match(regex);
+      const timeStampA = matchA ? parseInt(matchA[1]) : 0;
+      const timeStampB = matchB ? parseInt(matchB[1]) : 0;
+      return timeStampA - timeStampB;
+    })
+    for (let i = 0; i < files.length - MAX_FILE_NUM; i++) {
+      fs.unlinkSync(filePath + '/' + files[i]);
+      console.log(`File: ${files[i]} is deleted.`);
+    }
+  }  
+}
 
 let jsLeakWatcher = {
   watch: (obj, msg) => {
+    if (obj === undefined || obj === null || msg === undefined || msg === null) {
+      throw new BusinessError(ERROR_CODE_INVALID_PARAM);
+    }
     if (!enabled) {
       return;
     }
@@ -57,118 +129,62 @@ let jsLeakWatcher = {
       msg: msg
     };
     watchObjMap.set(objMsg.hash, objMsg);
-
-    const registry = new FinalizationRegistry((hash) => {
-      gcObjList.push(hash);
-    });
     registry.register(obj, objMsg.hash);
   },
   check: () => {
     if (!enabled) {
-      return;
+      return '';
     }
-    ArkTools.forceFullGC();
-    leakObjList.length = 0;
-    for (let [key, value] of watchObjMap) {
-      if (!gcObjList.includes(key)) {
-        leakObjList.push(value);
-      }
-    }
+    let leakObjList = flushLeakList();
     return JSON.stringify(leakObjList);
   },
   dump: (filePath) => {
+    if (filePath === undefined || filePath === null) {
+      throw new BusinessError(ERROR_CODE_INVALID_PARAM);
+    }
+    let fileArray = new Array(2);
     if (!enabled) {
-      return;
+      return fileArray;
     }
     if (!fs.accessSync(filePath, fs.AccessModeType.EXIST)) {
       throw new BusinessError(ERROR_CODE_INVALID_PARAM);
     }
 
-    const timeStamp = new Date().getTime().toString();
+    const fileTimeStamp = new Date().getTime().toString();
     try {
-      hidebug.dumpJsHeapData(timeStamp);
-      let heapDumpFileName = timeStamp + '.heapsnapshot';
-      fs.moveFileSync('/data/storage/el2/base/files/' + heapDumpFileName, filePath + '/' + heapDumpFileName, 0);
+      const heapDumpSHA256 = createHeapDumpFile(fileTimeStamp, filePath);
 
-      let file = fs.openSync(filePath + '/' + timeStamp + '.jsleaklist', fs.OpenMode.READ_WRITE | fs.OpenMode.CREATE);
-      let md = cryptoFramework.createMd('SHA256');
-      let heapDumpFile = fs.openSync(filePath + '/' + heapDumpFileName, fs.OpenMode.READ_WRITE);
-      let bufSize = 1024;
-      let readSize = 0;
-      let buf = new ArrayBuffer(bufSize);
-      let readOptions = {
-        offset: readSize,
-        length: bufSize
-      };
-      let readLen = fs.readSync(heapDumpFile.fd, buf, readOptions);
-
-      while (readLen > 0) {
-        md.updateSync({ data: new Uint8Array(buf.slice(0, readLen)) });
-        readSize += readLen;
-        readOptions.offset = readSize;
-        readLen = fs.readSync(heapDumpFile.fd, buf, readOptions);
-      }
-      fs.closeSync(heapDumpFile);
-
-      let digestOutPut = md.digestSync();
-      const hexString = Array.from(digestOutPut.data, byte => ('0' + byte.toString(16)).slice(-2))
-                        .join('').toUpperCase();
+      let file = fs.openSync(filePath + '/' + fileTimeStamp + '.jsleaklist', fs.OpenMode.READ_WRITE | fs.OpenMode.CREATE);
+      let leakObjList = flushLeakList();
       let result = {
-        snapshot_hash: hexString,
+        snapshot_hash: heapDumpSHA256,
         leakObjList: leakObjList
       };
       fs.writeSync(file.fd, JSON.stringify(result));
       fs.closeSync(file);
     } catch (error) {
       console.log('Dump heaoSnapShot or LeakList failed! ' + e);
-      return null;
+      return fileArray;
     }
 
     try {
-      let listFileOption = {
-        recursion: false,
-        listNum: 0,
-        filter: {
-          suffix: [".heapsnapshot", ".jsleaklist"],
-          fileSizeOver: 0
-        }
-      };
-      let files = fs.listFileSync(filePath, listFileOption);
-      if (files.length > MAX_FILE_NUM) {
-        const regex = /(\d+)\.(heapsnapshot|jsleaklist)/;
-        files.sort((a, b) => {
-          const matchA = a.match(regex);
-          const matchB = b.match(regex);
-          const timeStampA = matchA ? parseInt(matchA[1]) : 0;
-          const timeStampB = matchB ? parseInt(matchB[1]) : 0;
-          return timeStampA - timeStampB;
-        })
-        for (let i = 0; i < files.length - MAX_FILE_NUM; i++) {
-          fs.unlinkSync(filePath + '/' + files[i]);
-          console.log(`File: ${files[i]} is deleted.`);
-        }
-      }
+      deleteOldFile(filePath);
     } catch (e) {
       console.log('Delete old files failed! ' + e);
-      return null;
+      return fileArray;
     }
 
-    let fileArray = new Array(2);
-    let leakListFileName = timeStamp + '.jsleaklist';
-    let heapSnapShotFileName = timeStamp + '.heapsnapshot';
-    fileArray[0] = leakListFileName;
-    fileArray[1] = heapSnapShotFileName;
+    fileArray[0] = fileTimeStamp + '.jsleaklist';
+    fileArray[1] = fileTimeStamp + '.heapsnapshot';
     return fileArray;
   },
   enable: (isEnable) => {
-    if (typeof isEnable !== 'boolean') {
-      console.log('Parameter error!');
-      return;
-    } 
+    if (isEnable === undefined || isEnable === null) {
+      throw new BusinessError(ERROR_CODE_INVALID_PARAM);
+    }
     enabled = isEnable;
     if (!isEnable) {
       gcObjList.length = 0;
-      leakObjList.length = 0;
       watchObjMap.clear();
     }
   }
