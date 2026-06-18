ba45e577a39a447055a7f3651484094f9e46b629
gan-chuantao <ganchuantao1@h-partners.com>
2026-06-18 15:38:49 +0800

    添加打点
    Signed-off-by: gan-chuantao <ganchuantao1@h-partners.com>
    Change-Id: Iaa23e10209c82bb95105523cdde60ba7f2a92716

--- interfaces/js/kits/napi/js_leak_watcher/js_leak_watcher.ts
+++ interfaces/js/kits/napi/js_leak_watcher/js_leak_watcher.ts
@@ -786,11 +786,11 @@ let jsLeakWatcher = {
       return;
     }
     enabled = isEnabled;
+    jsLeakWatcherNative.apiRecord('enableLeakWatcher');
     if (!isEnabled) {
       shutdownJsLeakWatcher();
       return;
     }
-    jsLeakWatcherNative.apiRecord('enableLeakWatcher');
     getCustomAttribute(configs);
 
     const validConfig = ['CustomComponent', 'Window', 'NodeContainer', 'XComponent', 'Ability'];
